29d13bb272d7485bcd8b455e2b520b9ae59e9571
SindiswaN <st10240725@imconnect.edu.za>
2025-11-07 18:15:40 +0200

    configured sonar and circleci

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,6 @@
 // api.js
 import axios from "axios";
 
-// Temporary fix for development - ignore self-signed certificates
-if (process.env.NODE_ENV === 'development') {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-}
-
 const api = axios.create({
   baseURL: "https://localhost:5001",
   withCredentials: true,
